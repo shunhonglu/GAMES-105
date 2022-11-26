338a2df2ed65f39d475481b83c0688b31e02467e
shunhonglu <shunhonglu@gmail.com>
2022-11-26 14:39:43 +0800

    first commmit

--- lab1/Lab1_FK_answers.py
+++ lab1/Lab1_FK_answers.py
@@ -30,9 +30,39 @@ def part1_calculate_T_pose(bvh_file_path):
     Tips:
         joint_name顺序应该和bvh一致
     """
-    joint_name = None
-    joint_parent = None
-    joint_offset = None
+    joint_name = []
+    joint_parent = []
+    joint_offset = []
+
+    index = 0
+    indexStack = []
+    with open(bvh_file_path) as file_object:
+        for line in file_object:
+            words = line.split()
+            if words[0] == "MOTION":
+                assert len(indexStack) == 0
+                break
+            elif words[0] == "ROOT" or words[0] == "JOINT" or words[0] == "End":
+                if words[0] == "End":
+                    joint_name.append(joint_name[indexStack[-1]] + "_end")
+                else:
+                    joint_name.append(words[1])
+
+                if words[0] == "ROOT":
+                    joint_parent.append(-1)
+                else:
+                    joint_parent.append(indexStack[-1])
+
+                indexStack.append(index)
+                index += 1
+            elif words[0] == "OFFSET":
+                offset = [float(x) for x in words[1 : 4]]
+                joint_offset.append(offset)
+            elif words[0] == "}":
+                indexStack.pop()
+
+    joint_offset = np.array(joint_offset)
+    assert joint_offset.shape == (len(joint_name), 3)
     return joint_name, joint_parent, joint_offset
 
 
@@ -48,8 +78,27 @@ def part2_forward_kinematics(joint_name, joint_parent, joint_offset, motion_data
         1. joint_orientations的四元数顺序为(x, y, z, w)
         2. from_euler时注意使用大写的XYZ
     """
-    joint_positions = None
-    joint_orientations = None
+    joint_positions = np.zeros((len(joint_name), 3))
+    joint_orientations = np.zeros((len(joint_name), 4))
+
+    frame = motion_data[frame_id]
+
+    joint_positions[0] = frame[ : 3]
+    joint_orientations[0] = R.from_euler('XYZ', frame[3 : 6], degrees=True).as_quat()
+
+    j = 0
+    for i in range(1, len(joint_name)):
+        parent_postition = joint_positions[joint_parent[i]]
+        parent_orientation = R.from_quat(joint_orientations[joint_parent[i]])
+    
+        joint_positions[i] = parent_postition + parent_orientation.apply(joint_offset[i])
+        if not joint_name[i].endswith("_end"):
+            rotation = R.from_euler('XYZ', frame[6 + 3*j : 9 + 3*j], degrees=True)
+            joint_orientations[i] = (parent_orientation * rotation).as_quat()
+            j += 1
+        else:
+            joint_orientations[i] = joint_orientations[joint_parent[i]]
+
     return joint_positions, joint_orientations
 
 
@@ -63,5 +112,45 @@ def part3_retarget_func(T_pose_bvh_path, A_pose_bvh_path):
         两个bvh的joint name顺序可能不一致哦(
         as_euler时也需要大写的XYZ
     """
-    motion_data = None
+    motion_data = load_motion_data(A_pose_bvh_path)
+    motion_data_dict = {}
+
+    joint_name_T, joint_parent_T, joint_offset_T = part1_calculate_T_pose(T_pose_bvh_path)
+    joint_name_A, joint_parent_A, joint_offset_A = part1_calculate_T_pose(A_pose_bvh_path)
+
+    # T_pose → A_pose
+    lshoulder = R.from_euler('XYZ', [0, 0, -45], degrees=True)
+    lshoulder_T = R.from_matrix(lshoulder.as_matrix().T)
+    rshoulder = R.from_euler('XYZ', [0, 0, 45], degrees=True)
+    rshoulder_T = R.from_matrix(rshoulder.as_matrix().T)
+
+    j = 0
+    for joint_name in joint_name_A:
+        if joint_name == "RootJoint":
+            motion_data_dict[joint_name] = motion_data[:, 0 : 6]
+        elif joint_name.endswith("_end"):
+            continue
+        else:
+            if joint_name == "lShoulder":
+                motion_data_dict[joint_name] = (R.from_euler('XYZ', motion_data[:, 6 + 3*j : 9 + 3*j], degrees=True) * lshoulder).as_euler('XYZ', degrees=True)
+            elif joint_name == "lElbow" or joint_name == "lWrist":
+                motion_data_dict[joint_name] = (lshoulder_T * R.from_euler('XYZ', motion_data[:, 6 + 3*j : 9 + 3*j], degrees=True) * lshoulder).as_euler('XYZ', degrees=True)
+            elif joint_name == "rShoulder":
+                motion_data_dict[joint_name] = (R.from_euler('XYZ', motion_data[:, 6 + 3*j : 9 + 3*j], degrees=True) * rshoulder).as_euler('XYZ', degrees=True)
+            elif joint_name == "rElbow" or joint_name == "rWrist":
+                motion_data_dict[joint_name] = (rshoulder_T * R.from_euler('XYZ', motion_data[:, 6 + 3*j : 9 + 3*j], degrees=True) * rshoulder).as_euler('XYZ', degrees=True)
+            else:
+                motion_data_dict[joint_name] = motion_data[:, 6 + 3*j : 9 + 3*j]
+            j += 1
+
+    j = 0
+    for joint_name in joint_name_T:
+        if joint_name == "RootJoint":
+            motion_data[:, 0 : 6] = motion_data_dict[joint_name]
+        elif joint_name.endswith("_end"):
+            continue
+        else:
+            motion_data[:, 6 + 3*j : 9 + 3*j] = motion_data_dict[joint_name]
+            j += 1
+
     return motion_data
--- lab1/Lab2_IK_answers.py
+++ lab1/Lab2_IK_answers.py
@@ -14,7 +14,66 @@ def part1_inverse_kinematics(meta_data, joint_positions, joint_orientations, tar
         joint_positions: 计算得到的关节位置，是一个numpy数组，shape为(M, 3)，M为关节数
         joint_orientations: 计算得到的关节朝向，是一个numpy数组，shape为(M, 4)，M为关节数
     """
+    path, path_name, path1, path2 = meta_data.get_path_from_root_to_end() 
+    joint_name = meta_data.joint_name
+    # 这里不能直接给列表赋值。
+    joint_parent = meta_data.joint_parent.copy()
+    joint_initial_position = meta_data.joint_initial_position
+    end_joint = meta_data.end_joint
     
+    # 计算各个 joint 的偏移。
+    joint_offset = [joint_initial_position[i] - joint_initial_position[joint_parent[i]] for i in range(1, len(joint_name))]
+    joint_offset.insert(0, joint_initial_position[0])
+
+    # 调整 path2 上 joint 的父节点。
+    i = 0
+    joint_parent[path[i]] = -1
+    while path[i] != 0:
+        joint_parent[path[i + 1]] = path[i]
+        i += 1
+
+    dis = float("inf")
+    iter = 0
+    
+    path = list(reversed(path))
+    while iter < 30 and dis >= 0.01:
+        for index in path:
+            if joint_name[index] == end_joint:
+                continue
+            
+            rotIndex = index
+            if index in path2 and joint_parent[index] != -1:
+                rotIndex = joint_parent[index]
+            dirtSrc = joint_positions[joint_name.index(end_joint)] - joint_positions[rotIndex]
+            dirtDist = target_pose - joint_positions[rotIndex]
+            unit_dirtSrc = dirtSrc / np.linalg.norm(dirtSrc)
+            unit_dirtDist = dirtDist / np.linalg.norm(dirtDist)
+            rotAngle = np.arccos(np.dot(unit_dirtSrc, unit_dirtDist))
+            rotAxis = np.cross(dirtSrc, dirtDist)
+            rotAxis = rotAxis / np.linalg.norm(rotAxis)
+            rot = R.from_rotvec(rotAngle * rotAxis)
+
+            joint_orientations[index] = (rot * R.from_quat(joint_orientations[index])).as_quat()
+            if index in path2 and joint_parent[index] != -1:
+                joint_positions[index] = joint_positions[joint_parent[index]] - R.from_quat(joint_orientations[index]).apply(joint_offset[joint_parent[index]])
+                for j in path2[path2.index(index) + 1 : ]:
+                    joint_orientations[j] = (rot * R.from_quat(joint_orientations[j])).as_quat()
+                    joint_positions[j] = joint_positions[joint_parent[j]] - R.from_quat(joint_orientations[j]).apply(joint_offset[joint_parent[j]]) 
+            
+            for j in range(len(joint_name)):
+                if j in path2:
+                    continue
+                parent = joint_parent[j]
+                while parent != -1:
+                    if parent != index:
+                        parent = joint_parent[parent]
+                    else:
+                        joint_orientations[j] = (rot * R.from_quat(joint_orientations[j])).as_quat()
+                        joint_positions[j] = joint_positions[joint_parent[j]] + R.from_quat(joint_orientations[joint_parent[j]]).apply(joint_offset[j])
+                        break
+
+        dis = np.linalg.norm(target_pose - joint_positions[path[0]])
+        iter += 1
     
     return joint_positions, joint_orientations
 
@@ -22,7 +81,57 @@ def part2_inverse_kinematics(meta_data, joint_positions, joint_orientations, rel
     """
     输入lWrist相对于RootJoint前进方向的xz偏移，以及目标高度，IK以外的部分与bvh一致
     """
+    path, path_name, path1, path2 = meta_data.get_path_from_root_to_end() 
+    joint_name = meta_data.joint_name
+    joint_parent = meta_data.joint_parent
+    joint_initial_position = meta_data.joint_initial_position
+    end_joint = meta_data.end_joint
+
+    joint_offset = [joint_initial_position[i] - joint_initial_position[joint_parent[i]] for i in range(1, len(joint_name))]
+    joint_offset.insert(0, joint_initial_position[0])
+
+    # 是从上一个位置开始！
+    target_position = joint_positions[joint_name.index("RootJoint")] + [relative_x, 0, relative_z]
+    target_position[1] = target_height
+
+    dis = float("inf")
+    iter = 0
     
+    path = list(reversed(path))
+    while iter < 10 and dis >= 0.01:
+        for index in path:
+            if joint_name[index] == end_joint:
+                continue
+            
+            rotIndex = index
+            if index in path2 and joint_parent[index] != -1:
+                rotIndex = joint_parent[index]
+            dirtSrc = joint_positions[joint_name.index(end_joint)] - joint_positions[rotIndex]
+            dirtDist = target_position - joint_positions[rotIndex]
+            unit_dirtSrc = dirtSrc / np.linalg.norm(dirtSrc)
+            unit_dirtDist = dirtDist / np.linalg.norm(dirtDist)
+            rotAngle = np.arccos(np.dot(unit_dirtSrc, unit_dirtDist))
+            rotAxis = np.cross(dirtSrc, dirtDist)
+            rotAxis = rotAxis / np.linalg.norm(rotAxis)
+            rot = R.from_rotvec(rotAngle * rotAxis)
+
+            joint_orientations[index] = (rot * R.from_quat(joint_orientations[index])).as_quat()
+            if joint_parent[index] != -1:
+                joint_positions[index] = joint_positions[joint_parent[index]] + R.from_quat(joint_orientations[joint_parent[index]]).apply(joint_offset[index])
+            
+            for j in range(len(joint_name)):
+                parent = joint_parent[j]
+                while parent != -1:
+                    if parent != index:
+                        parent = joint_parent[parent]
+                    else:
+                        joint_orientations[j] = (rot * R.from_quat(joint_orientations[j])).as_quat()
+                        joint_positions[j] = joint_positions[joint_parent[j]] + R.from_quat(joint_orientations[joint_parent[j]]).apply(joint_offset[j])
+                        break
+
+        dis = np.linalg.norm(target_position - joint_positions[path[0]])
+        iter += 1
+
     return joint_positions, joint_orientations
 
 def bonus_inverse_kinematics(meta_data, joint_positions, joint_orientations, left_target_pose, right_target_pose):
